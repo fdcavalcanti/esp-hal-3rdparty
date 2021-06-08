fdcavalcanti/esp-hal-3rdparty

commit 0bc4524555f8ee7a5f034f2929c95fa012854f2f

esp32: move esp_clk functions

components/esp_hw_support/esp_clk.c
@@ -1,0 +1,139 @@
+// Copyright 2015-2017 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <stdint.h>
+#include <sys/param.h>
+
+#include "esp_attr.h"
+#include "soc/rtc.h"
+
+#if CONFIG_IDF_TARGET_ESP32
+#include "esp32/rom/rtc.h"
+#include "esp32/clk.h"
+#include "esp32/rtc.h"
+#elif CONFIG_IDF_TARGET_ESP32S2
+#include "esp32s2/rom/rtc.h"
+#include "esp32s2/clk.h"
+#include "esp32s2/rtc.h"
+#elif CONFIG_IDF_TARGET_ESP32S3
+#include "esp32s3/rom/rtc.h"
+#include "esp32s3/clk.h"
+#include "esp32s3/rtc.h"
+#include "esp32s3/rom/ets_sys.h"
+#elif CONFIG_IDF_TARGET_ESP32C3
+#include "esp32c3/rom/rtc.h"
+#include "esp32c3/clk.h"
+#include "esp32c3/rtc.h"
+#endif
+
+#define MHZ (1000000)
+
+// g_ticks_us defined in ROMs for PRO and APP CPU
+extern uint32_t g_ticks_per_us_pro;
+#if CONFIG_IDF_TARGET_ESP32
+#ifndef CONFIG_FREERTOS_UNICORE
+extern uint32_t g_ticks_per_us_app;
+#endif
+#endif
+
+static _lock_t s_esp_rtc_time_lock;
+static RTC_DATA_ATTR uint64_t s_esp_rtc_time_us = 0, s_rtc_last_ticks = 0;
+
+int IRAM_ATTR esp_clk_cpu_freq(void)
+{
+#if CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32S3
+    return ets_get_cpu_frequency() * MHZ;
+#else
+    return g_ticks_per_us_pro * MHZ;
+#endif
+}
+
+int IRAM_ATTR esp_clk_apb_freq(void)
+{
+#if CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32S3
+    return MIN(ets_get_cpu_frequency(), 81) * MHZ;
+#else
+    return MIN(g_ticks_per_us_pro, 80) * MHZ;
+#endif
+}
+
+int IRAM_ATTR esp_clk_xtal_freq(void)
+{
+    return rtc_clk_xtal_freq_get() * MHZ;
+}
+
+#ifndef CONFIG_IDF_TARGET_ESP32C3
+void IRAM_ATTR ets_update_cpu_frequency(uint32_t ticks_per_us)
+{
+    /* Update scale factors used by esp_rom_delay_us */
+    g_ticks_per_us_pro = ticks_per_us;
+#if CONFIG_IDF_TARGET_ESP32
+#ifndef CONFIG_FREERTOS_UNICORE
+    g_ticks_per_us_app = ticks_per_us;
+#endif
+#endif
+}
+#endif
+
+uint64_t esp_rtc_get_time_us(void)
+{
+    _lock_acquire(&s_esp_rtc_time_lock);
+    const uint32_t cal = esp_clk_slowclk_cal_get();
+    const uint64_t rtc_this_ticks = rtc_time_get();
+    const uint64_t ticks = rtc_this_ticks - s_rtc_last_ticks;
+    /* RTC counter result is up to 2^48, calibration factor is up to 2^24,
+     * for a 32kHz clock. We need to calculate (assuming no overflow):
+     *   (ticks * cal) >> RTC_CLK_CAL_FRACT
+     *
+     * An overflow in the (ticks * cal) multiplication would cause time to
+     * wrap around after approximately 13 days, which is probably not enough
+     * for some applications.
+     * Therefore multiplication is split into two terms, for the lower 32-bit
+     * and the upper 16-bit parts of "ticks", i.e.:
+     *   ((ticks_low + 2^32 * ticks_high) * cal) >> RTC_CLK_CAL_FRACT
+     */
+    const uint64_t ticks_low = ticks & UINT32_MAX;
+    const uint64_t ticks_high = ticks >> 32;
+    const uint64_t delta_time_us = ((ticks_low * cal) >> RTC_CLK_CAL_FRACT) +
+           ((ticks_high * cal) << (32 - RTC_CLK_CAL_FRACT));
+    s_esp_rtc_time_us += delta_time_us;
+    s_rtc_last_ticks = rtc_this_ticks;
+    _lock_release(&s_esp_rtc_time_lock);
+    return s_esp_rtc_time_us;
+}
+
+void esp_clk_slowclk_cal_set(uint32_t new_cal)
+{
+#if defined(CONFIG_ESP_TIME_FUNCS_USE_RTC_TIMER)
+    /* To force monotonic time values even when clock calibration value changes,
+     * we adjust esp_rtc_time
+     */
+    esp_rtc_get_time_us();
+#endif // CONFIG_ESP_TIME_FUNCS_USE_RTC_TIMER
+    REG_WRITE(RTC_SLOW_CLK_CAL_REG, new_cal);
+}
+
+uint32_t esp_clk_slowclk_cal_get(void)
+{
+    return REG_READ(RTC_SLOW_CLK_CAL_REG);
+}
+
+uint64_t esp_clk_rtc_time(void)
+{
+#ifdef CONFIG_ESP_TIME_FUNCS_USE_RTC_TIMER
+    return esp_rtc_get_time_us();
+#else
+    return 0;
+#endif
+}

components/bt/controller/esp32c3/bt.c
@@ -40,7 +40,7 @@
 #include "soc/rtc.h"
 #include "soc/rtc_cntl_reg.h"
 #include "soc/soc_memory_layout.h"
-#include "esp_clk.h"
+#include "esp32c3/clk.h"
 #include "esp_coexist_internal.h"
 #include "esp32c3/rom/rom_layout.h"
 #include "esp_timer.h"

components/bt/controller/esp32s3/bt.c
@@ -41,7 +41,7 @@
 #include "soc/rtc.h"
 #include "soc/rtc_cntl_reg.h"
 #include "soc/soc_memory_layout.h"
-#include "esp_clk.h"
+#include "esp32c3/clk.h"
 #include "esp_coexist_internal.h"
 
 #if CONFIG_BT_ENABLED

components/esp_hw_support/CMakeLists.txt
@@ -7,11 +7,11 @@ endif()
 
 set(srcs "compare_set.c" "cpu_util.c")
 if(NOT BOOTLOADER_BUILD)
-    list(APPEND srcs "clk_ctrl_os.c" "mac_addr.c" "hw_random.c")
+    list(APPEND srcs "esp_clk.c" "clk_ctrl_os.c" "mac_addr.c" "hw_random.c")
 endif()
 
 idf_component_register(SRCS ${srcs}
-                       INCLUDE_DIRS include
+                       INCLUDE_DIRS include include/soc
                        REQUIRES ${requires}
                        PRIV_REQUIRES efuse
                        LDFRAGMENTS linker.lf)

components/esp_hw_support/component.mk
@@ -1,5 +1,5 @@
 COMPONENT_SRCDIRS := . port/$(IDF_TARGET)
-COMPONENT_ADD_INCLUDEDIRS := . include port/$(IDF_TARGET)/private_include
+COMPONENT_ADD_INCLUDEDIRS := . include include/soc port/$(IDF_TARGET)/private_include
 COMPONENT_ADD_LDFRAGMENTS := linker.lf
 
 port/$(IDF_TARGET)/rtc_clk.o: CFLAGS += -fno-jump-tables -fno-tree-switch-conversion

components/esp_hw_support/include/esp_clk.h
@@ -1,0 +1,2 @@
+#warning esp_clk.h has been replaced by esp32/clk.h, please include esp32/clk.h instead
+#include "esp32/clk.h"

components/esp_hw_support/include/esp_private/esp_clk.h
@@ -1,0 +1,95 @@
+// Copyright 2015-2017 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @file esp_clk.h
+ *
+ * This file contains declarations of clock related functions.
+ */
+
+/**
+ * @brief Get the calibration value of RTC slow clock
+ *
+ * The value is in the same format as returned by rtc_clk_cal (microseconds,
+ * in Q13.19 fixed-point format).
+ *
+ * @return the calibration value obtained using rtc_clk_cal, at startup time
+ */
+uint32_t esp_clk_slowclk_cal_get(void);
+
+/**
+ * @brief Update the calibration value of RTC slow clock
+ *
+ * The value has to be in the same format as returned by rtc_clk_cal (microseconds,
+ * in Q13.19 fixed-point format).
+ * This value is used by timekeeping functions (such as gettimeofday) to
+ * calculate current time based on RTC counter value.
+ * @param value calibration value obtained using rtc_clk_cal
+ */
+void esp_clk_slowclk_cal_set(uint32_t value);
+
+/**
+ * @brief Return current CPU clock frequency
+ * When frequency switching is performed, this frequency may change.
+ * However it is guaranteed that the frequency never changes with a critical
+ * section.
+ *
+ * @return CPU clock frequency, in Hz
+ */
+int esp_clk_cpu_freq(void);
+
+/**
+ * @brief Return current APB clock frequency
+ *
+ * When frequency switching is performed, this frequency may change.
+ * However it is guaranteed that the frequency never changes with a critical
+ * section.
+ *
+ * @return APB clock frequency, in Hz
+ */
+int esp_clk_apb_freq(void);
+
+/**
+ * @brief Return frequency of the main XTAL
+ *
+ * Frequency of the main XTAL can be either auto-detected or set at compile
+ * time (see CONFIG_ESP32_XTAL_FREQ_SEL sdkconfig option). In both cases, this
+ * function returns the actual value at run time.
+ *
+ * @return XTAL frequency, in Hz
+ */
+int esp_clk_xtal_freq(void);
+
+
+/**
+ * @brief Read value of RTC counter, converting it to microseconds
+ * @attention The value returned by this function may change abruptly when
+ * calibration value of RTC counter is updated via esp_clk_slowclk_cal_set
+ * function. This should not happen unless application calls esp_clk_slowclk_cal_set.
+ * In ESP-IDF, esp_clk_slowclk_cal_set is only called in startup code.
+ *
+ * @return Value or RTC counter, expressed in microseconds
+ */
+uint64_t esp_clk_rtc_time(void);
+
+#ifdef __cplusplus
+}
+#endif

components/esp_hw_support/include/soc/esp32/clk.h
@@ -1,0 +1,16 @@
+// Copyright 2015-2017 Espressif Systems (Shanghai) PTE LTD
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+#include "esp_private/esp_clk.h"

commit 0b0f84b343161e938361f8359e0c8a003234dfef

esp_hw_support: Fix ESP32-C3/S3 APB frequency
Regression in 0bc4524555f8ee7a5f034f2929c95fa012854f2f, APB frequency
accidentally set at 81MHz not 80MHz.

components/esp_hw_support/esp_clk.c
@@ -50,22 +50,23 @@ extern uint32_t g_ticks_per_us_app;
 static _lock_t s_esp_rtc_time_lock;
 static RTC_DATA_ATTR uint64_t s_esp_rtc_time_us = 0, s_rtc_last_ticks = 0;
 
-int IRAM_ATTR esp_clk_cpu_freq(void)
+inline static int IRAM_ATTR s_get_cpu_freq_mhz(void)
 {
 #if CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32S3
-    return ets_get_cpu_frequency() * MHZ;
+    return ets_get_cpu_frequency();
 #else
-    return g_ticks_per_us_pro * MHZ;
+    return g_ticks_per_us_pro;
 #endif
+}
+
+int IRAM_ATTR esp_clk_cpu_freq(void)
+{
+    return s_get_cpu_freq_mhz() * MHZ;
 }
 
 int IRAM_ATTR esp_clk_apb_freq(void)
 {
-#if CONFIG_IDF_TARGET_ESP32C3 || CONFIG_IDF_TARGET_ESP32S3
-    return MIN(ets_get_cpu_frequency(), 81) * MHZ;
-#else
-    return MIN(g_ticks_per_us_pro, 80) * MHZ;
-#endif
+    return MIN(s_get_cpu_freq_mhz(), 80) * MHZ;
 }
 
 int IRAM_ATTR esp_clk_xtal_freq(void)