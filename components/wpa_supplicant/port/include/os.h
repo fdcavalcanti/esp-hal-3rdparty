84b8b0a371bc4da29172b7614c4b1be28414df18
chenwen@espressif.com <chenwen@espressif.com>
2024-01-02 15:40:21 +0800

    feat(wifi): Support WPA3 on softap mode
    Signed-off-by: chenwen@espressif.com <chenwen@espressif.com>

--- components/wpa_supplicant/port/include/os.h
+++ components/wpa_supplicant/port/include/os.h
@@ -27,6 +27,7 @@
 #include "supplicant_opt.h"
 #include "esp_private/esp_wifi_private.h"
 #include "esp_wifi.h"
+#include "esp_private/wifi.h"
 
 /* Modifying datatype for platform and compiler independence */
 
@@ -43,6 +44,11 @@ struct os_time {
     os_time_t sec;
     suseconds_t usec;
 };
+
+#ifdef __NuttX__
+typedef void*           TaskHandle_t;
+typedef void*           SemaphoreHandle_t;
+#endif
 
 #define os_reltime os_time
 
@@ -233,6 +239,18 @@ static inline char *os_readfile(const char *name, size_t *len)
 
 #ifndef os_free
 #define os_free(p) kmm_free((p))
+#endif
+
+#ifndef pdTRUE
+#define pdTRUE              (1)
+#endif
+
+#ifndef pdPASS
+#define pdPASS              (pdTRUE)
+#endif
+
+#ifndef portMAX_DELAY
+#define portMAX_DELAY       OSI_FUNCS_TIME_BLOCKING
 #endif
 
 #else
